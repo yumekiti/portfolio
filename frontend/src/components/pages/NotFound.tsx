311988098b83e63725c802bf038d7b08aa5fc889
yumekiti <yumekiti120440@gmail.com>
2024-02-29 05:00:21 +0900

    chore: update container padding in Home and NotFound pages

--- frontend/src/components/pages/NotFound.tsx
+++ frontend/src/components/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/templates/Layout";
 const Component = () => {
   return (
     <Layout>
-      <div className="w-full xl:w-[40vw] container mx-auto py-8 space-y-8">
+      <div className="w-full xl:w-[40vw] container mx-auto py-24 space-y-8">
         <p>404</p>
       </div>
     </Layout>
