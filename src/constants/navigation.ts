6672f8a8b7bcf5d4d893e8892a636b6316af5245
jp43965 <jp43965@linecorp.com>
2024-04-19 10:28:47 +0900

    feat: add tailwind

---
+++ src/constants/navigation.ts
@@ -0,0 +1,19 @@
+const navigation = [
+    {
+      icon: '🏠',
+      path: '/',
+    },
+    {
+      emoji: '🎉',
+      path: '/blog',
+    },
+    {
+      emoji: '❓',
+      path: '/github',
+    },
+    {
+      emoji: '❓',
+      path: '/twitter',
+    },
+  ];
+  
