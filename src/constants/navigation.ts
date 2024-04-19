9cebab4d653806ee015e6ee82c296603c7a394e4
jp43965 <jp43965@linecorp.com>
2024-04-19 10:38:26 +0900

    chore: format code

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -1,19 +1,18 @@
 const navigation = [
-    {
-      icon: '🏠',
-      path: '/',
-    },
-    {
-      emoji: '🎉',
-      path: '/blog',
-    },
-    {
-      emoji: '❓',
-      path: '/github',
-    },
-    {
-      emoji: '❓',
-      path: '/twitter',
-    },
-  ];
-  
+  {
+    icon: "🏠",
+    path: "/",
+  },
+  {
+    emoji: "🎉",
+    path: "/blog",
+  },
+  {
+    emoji: "❓",
+    path: "/github",
+  },
+  {
+    emoji: "❓",
+    path: "/twitter",
+  },
+];
