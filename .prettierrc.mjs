3d4357d3225605b10ab39a9289ae0728730bae36
jp43965 <jp43965@linecorp.com>
2024-04-19 10:15:41 +0900

    chore: add prettier

---
+++ .prettierrc.mjs
@@ -0,0 +1,25 @@
+// .prettierrc.mjs
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: true,
+  plugins: ["prettier-plugin-astro"],
+  overrides: [
+    {
+      files: [".*", "*.md", "*.mdx", "*.toml", "*.yml"],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: ["**/*.astro"],
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
