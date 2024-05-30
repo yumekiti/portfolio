4dd8415416fe02104edc51a48cd2e2375fdb170e
yumekiti <yumekiti120440@gmail.com>
2024-05-31 00:52:50 +0900

    refactor: responsive design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
@@ -6,4 +6,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
+  image: {
+    service: passthroughImageService(),
+  },
 });
